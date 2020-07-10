4ec369c1bdc76a57d5eebdbb8abbebec84d9f8c3
njahan98 <51012116+njahan98@users.noreply.github.com>
2020-07-10 01:14:58 -0400

    Add files via upload
    player versus computer in rock, paper, scissors. Randint is used to produce answers from computer.

---
+++ v3.py
@@ -0,0 +1,40 @@
+##randint returns a random integer N between a & b 
+##generate between 0-2 
+
+from random import randint
+
+player = input("Player, make your move! ").lower()
+
+rand_num = randint(0,2)
+if rand_num == 0:
+	computer = "rock"
+elif rand_num == 1:
+	computer = "paper"
+else:
+	computer = "scissors"
+
+print(f"Computer plays {computer}")
+
+if player == computer:
+	print("It's a tie!")
+
+elif player == "rock":
+	if computer == "scissors":
+		print("player wins!")
+	elif computer == "paper":
+		print("computer wins!")
+
+elif player == "paper":
+	if computer == "rock":
+		print("player wins!")
+	elif computer == "scissors":
+		print("computer wins!")
+
+elif player == "scissors":
+	if computer == "paper":
+		print("player wins!")
+	elif computer == "rock":
+		print("computer wins!")
+
+else:
+	print("Please enter a valid move!")
